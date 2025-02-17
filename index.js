1d563d3024c87f85777a0d46e123dcfee6b025e1
Muhammad Usman <muhammad.usman@bitflex.com>
2025-02-17 14:18:48 +0500

    Headers and status code added with post and get requests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 
 //Create instance of express
@@ -7,15 +8,26 @@ const app = express();
 //Properties
 const Port = 8000;
 
-//Define Routes individually
-//---Get Request---
+//Middle ware
+app.use(express.urlencoded({ extended: false }));
 
-//--For mobile applications--
+app.use((req, res, next) => {
+  //prepare a log
+  fs.appendFile("log.txt", `\n ${Date.now}: ${req.method}: ${req.path}`);
+  next();
+});
+
+//Define Routes individually
+
+//---Get Request---
+//- 1 For mobile applications--
 app.get("/api/users", (req, res) => {
+  res.setHeaders("X-MyName", "Usman"); //"X at the start means custom header"
+  console.log(req.header);
   return res.json(users);
 });
 
-//Get user by dynamic id
+//- 2 Get user by dynamic id
 app.get("/api/users/:id", (req, res) => {
   const id = Number(req.params.id);
   const user = users.find((user) => user.id === id);
@@ -33,9 +45,30 @@ app.get("/users", (req, res) => {
 });
 
 //---Post Request---
+//- 1 For web
 app.post("/users", (req, res) => {
-  //TODO: Create new user
-  return res.json({ Status: Pending });
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  console.log("Body:", body);
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.status(201).json({ status: "Success", id: users.length });
+  });
+});
+
+//- 2 For mobile
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title ||
+    !body.last_name
+  ) {
+    res.status(400).json({ msg: "All Fields are required..." });
+  }
+  return res.json({ status: "Pending" });
 });
 
 /*
@@ -44,14 +77,25 @@ Methods
 */
 
 app
-  .route("/users:id")
+  .route("/users/:id")
   .patch((req, res) => {
     //TODO: Edit user
-    return res.json({ Status: Pending });
+    return res.json({ status: "Pending" });
   })
   .delete((req, res) => {
     //TODO: Delete user
-    return res.json({ Status: Pending });
+    const id = Number(req.params.id);
+    const filterUsers = users.filter((user) => user.id !== id);
+    fs.writeFile(
+      "./MOCK_DATA.json",
+      JSON.stringify(filterUsers, null, 2),
+      (err, data) => {
+        if (err) {
+          return res.status(500).json({ error: "Failed to delete user" });
+        }
+        res.json({ status: "Deleted Successfully", id: id });
+      }
+    );
   })
   .get((req, res) => {
     const id = Number(req.params.id);
